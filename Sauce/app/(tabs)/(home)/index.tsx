c3e8cada8f5b4becb0e7e0e941e9557fff373dd8
Densel Aquino <aquinods@g.cofc.edu>
2026-04-08 20:10:48 -0400

    Implementation of AsyncStorage along with more visuals to the home page.

--- Sauce/app/(tabs)/(home)/index.tsx
+++ Sauce/app/(tabs)/(home)/index.tsx
@@ -1,18 +1,85 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TouchableOpacity, StyleSheet} from 'react-native'
 import { useRouter } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Home() {
   const router = useRouter()
 
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+    {/* Header Row */}
+    <View style={styles.headerRow}>
+      <Text style={styles.headText}>Welcome, User!</Text>
 
-      <Button
-        title="Go to settings"
-        onPress={() => router.push('../(home)/settings')}
-      />
-
+      <TouchableOpacity
+        onPress={() => router.push("../(home)/settings")}
+        style={styles.iconButton}
+      >
+        <Ionicons name="settings-sharp" size={32} color="black" />
+      </TouchableOpacity>
     </View>
-  )
+
+    <Text style={styles.introText}>
+      Discover local restaurant's in the Charleston Area!
+    </Text>
+
+    <TouchableOpacity
+        style={styles.searchButton}
+        onPress={() => router.push("../search")}
+      >
+        <Text style={styles.searchText}>Search</Text>
+      </TouchableOpacity>
+
+    <Text style={styles.locationText}> Restaurant Name </Text>
+
+  </View>
+);
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  paddingTop: 20,
+  paddingHorizontal: 15,
+},
+headerRow: {
+  flexDirection: 'row', 
+  justifyContent: 'space-between', 
+  alignItems: 'center', 
+  paddingBottom: 20,
+},
+headText: {
+  fontSize: 40,
+  fontStyle: 'italic',
+},
+iconButton: {
+  padding: 5, 
+},
+introText: {
+  fontSize: 20,
+},
+searchButton: {
+  backgroundColor: '#660000', 
+  borderRadius: 180,
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 250,
+  height: 250,
+  marginTop: 30,
+  alignSelf: 'center',
+  borderColor: 'black',
+  borderWidth: 4
+ 
+},
+searchText: {
+  color: 'white',
+  fontSize: 32,
+  fontWeight: 'bold',
+},
+
+locationText:{
+  fontSize:20,
+  marginTop: 40,
+},
+
+});
---
+++ Sauce/database/favorites.ts
@@ -0,0 +1,44 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FAVORITES_KEY } from "@/constants/keys";
+import { Location } from "@/interfaces/interfaces";
+
+// Getting Favorites
+export const getFavorites = async(): Promise<Location[]> => {
+    const data = await AsyncStorage.getItem(FAVORITES_KEY);
+    return data ? JSON.parse(data) : [];
+};
+
+// Saving Favs
+export const saveFavorites = async (favorites: Location[]) => {
+    await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+};
+
+// Toggle Favs
+export const toggleFavorite = async (location: Location) => {
+    const favorites = await getFavorites();
+
+    const exist = favorites.find(f => f.id === location.id);
+
+    let updated;
+
+    if (exist) {
+     updated = favorites.filter(f => f.id !== location.id);
+    } else {
+     updated = [...favorites, location];
+    }
+
+    await saveFavorites(updated)
+    return updated;
+}
+
+
+
+
+
+
+
+
+
+
+
+
